4d11f78d4df7c90bbdd6a825528eaf6b71b54cfe
user <user@mail.ru>
2023-12-05 22:04:23 +0300

    lesson_4 done

--- lesson_3/food/food/urls.py
+++ lesson_3/food/food/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('app_food.urls'))
+    path('', include('app_food.urls')),
+    path('restauraunt/', include('app_meal.urls'))
+
 ]
